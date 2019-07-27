c6846fc15962a61e95bdd4917e471e77671397aa
ALVARLEUNG <alvar.leung@qq.com>
2019-07-27 11:25:57 +0800

    feat:init backend

---
+++ src/main/java/com/supercool/supercool_ipds_backend/SupercoolIpdsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.supercool.supercool_ipds_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupercoolIpdsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupercoolIpdsBackendApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/supercool/supercool_ipds_backend/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.supercool.supercool_ipds_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public String test () {
+        return "Hello Super Cooler!!!";
+    }
+}
