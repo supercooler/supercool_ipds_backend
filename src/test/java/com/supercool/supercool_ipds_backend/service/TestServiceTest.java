e3e72247117d1af1934c17870fa0b6ae6475358b
ALVARLEUNG <alvar.leung@qq.com>
2019-07-27 16:38:55 +0800

    [LST]refactor: update dependencies for build.gradle

--- src/test/java/com/supercool/supercool_ipds_backend/service/TestServiceTest.java
+++ src/test/java/com/supercool/supercool_ipds_backend/service/TestServiceTest.java
@@ -1,18 +1,16 @@
 package com.supercool.supercool_ipds_backend.service;
 
+
 import com.supercool.supercool_ipds_backend.common.exception.CustomException;
-import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
 public class TestServiceTest {
-
     /**
      * Test for ResponseEntity & Controller Advice
      */
@@ -26,7 +24,7 @@ public class TestServiceTest {
     @Test
     public void should_rerun_responseEntity_when_call_TestService () {
         ResponseEntity responseEntity = testService.testResult();
-        Assert.assertEquals(200, responseEntity.getStatusCodeValue());
+        Assertions.assertEquals(200, responseEntity.getStatusCodeValue());
     }
 
     @Test
