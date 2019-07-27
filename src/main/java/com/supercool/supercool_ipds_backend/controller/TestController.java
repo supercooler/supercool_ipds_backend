84491c02540f38c356e5c20429b1d92ff463ef0f
FelicityChen <felicity.chen@oocl.com>
2019-07-27 14:05:51 +0800

    feat: add cross origin config and test

--- src/main/java/com/supercool/supercool_ipds_backend/controller/TestController.java
+++ src/main/java/com/supercool/supercool_ipds_backend/controller/TestController.java
@@ -1,15 +1,29 @@
 package com.supercool.supercool_ipds_backend.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/test")
+@CrossOrigin(origins = "*", maxAge = 1000L)
 public class TestController {
 
     @GetMapping
     public String test () {
         return "Hello Super Cooler!!!";
+    }
+
+    @PostMapping
+    public String testPost (@RequestParam String param) {
+        return param;
+    }
+
+    @DeleteMapping
+    public String testDelete () {
+        return "test delete";
+    }
+
+    @PutMapping
+    public String testPut() {
+        return "test put";
     }
 }
