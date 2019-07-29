6839764a77345752bb4ce829041501e40be7db54
兆兆桑 <3244188372@qq.com>
2019-07-29 14:33:39 +0800

    [jerryLi] fix : add foreign key to lot and boy

--- src/main/resources/db/migration/V4__Add_ForeignKey_Lot_Boy.sql
+++ src/main/resources/db/migration/V4__Add_ForeignKey_Lot_Boy.sql
@@ -1,2 +1,1 @@
-ALTER TABLE parking_lot ADD parking_boy_id bigint UNSIGNED NOT NULL DEFAULT 0;
 ALTER TABLE parking_lot ADD CONSTRAINT parking_boy_lot FOREIGN KEY (parking_boy_id) REFERENCES parking_boy(id);
