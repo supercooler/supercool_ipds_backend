597b14adee7e379583618d3b52ad0cc114593d02
兆兆桑 <3244188372@qq.com>
2019-07-30 10:16:09 +0800

    Update V6__Add_ForeignKey_Order_Boy.sql

--- src/main/resources/db/migration/V6__Add_ForeignKey_Order_Boy.sql
+++ src/main/resources/db/migration/V6__Add_ForeignKey_Order_Boy.sql
@@ -1,1 +1,1 @@
-alter table parking_order add constraint order_parking_boy foreign key (parking_boy_id) references parking_boy;
+alter table parking_order add constraint order_parking_boy foreign key (parking_boy_id) references parking_boy(id);
