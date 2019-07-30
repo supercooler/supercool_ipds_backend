cc9691ca05881184726398cefa7eff3a404b2558
FelicityChen <felicity.chen@oocl.com>
2019-07-30 15:26:18 +0800

    [Felicity Chen]feat: add parking order const status in Constant.java

--- src/main/java/com/supercool/supercool_ipds_backend/common/constant/Constant.java
+++ src/main/java/com/supercool/supercool_ipds_backend/common/constant/Constant.java
@@ -20,4 +20,12 @@ public class Constant {
 
     // manager exception
     public static final String DELETE_NOT_FOUND_EXCEPTION_CODE = "20001";
+
+    // parking order statu
+    public static final String HAD_ORDERED = "已下单";
+    public static final String HAD_DISPATCHED = "已配单";
+    public static final String HAD_CAR_STOPED = "已停车";
+    public static final String IN_FETCHING_CAR = "取车中";
+    public static final String WILL_BE_CONFIRMED = "待确认";
+    public static final String FINISH_FETCHING = "已完成";
 }
