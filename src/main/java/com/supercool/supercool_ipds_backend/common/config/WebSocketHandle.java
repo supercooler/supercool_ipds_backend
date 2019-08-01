7da0d4ad4d3ce6ac539f2960b417963211a5eeb0
tjslzz <3244188372@qq.com>
2019-08-01 09:40:25 +0800

    [jerryLi] fix  : fix bugs of cross orign

--- src/main/java/com/supercool/supercool_ipds_backend/common/config/WebSocketHandle.java
+++ src/main/java/com/supercool/supercool_ipds_backend/common/config/WebSocketHandle.java
@@ -2,11 +2,13 @@ package com.supercool.supercool_ipds_backend.common.config;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 1000L)
 public class WebSocketHandle {
 
     @Autowired
